e2e648fd69d43eec826fb8c8150da8461894b60e
limchaehwan <chlim20@naver.com>
2025-08-25 21:46:14 +0900

    javascript prac

---
+++ chapter04.js
@@ -0,0 +1,12 @@
+// 01. 변수
+let age = 27;
+
+age = 30;
+console.log(age);
+
+// 02. 상수
+const birth = "1997.01.07";
+
+// 03 변수 네이밍 규칙
+let $_name;
+let name1;
---
+++ chapter05.js
@@ -0,0 +1,27 @@
+// 1. number type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+
+// 2. Sring
+let myname = "임채환";
+let myLocation = "광주";
+let introduce = myname + myLocation;
+let introducetext = `${myname}은 ${myLocation}에 거주합니다`;
+
+// 3. Boolean
+let isSwitchOn = true;
+
+// 4. Null(아무것도없다)
+let empty = null;
+
+// 명시적 형번환
+let str1 = "10";
+let strToNum1 = Number(str1);
+console.log(10 + strToNum1);
+
+let num11 = 20;
+let numToString = String(num11);
+console.log(numToString + "입니다");
